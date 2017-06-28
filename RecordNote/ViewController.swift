1cef2e690d8a33ea798f814f4429ea503b306fb3
aaron.zheng <aaronzheng@kugou.net>
2017-06-28 20:29:12 +0800

    Initial Commit

---
+++ RecordNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordNote
+//
+//  Created by aaron.zheng on 2017-06-28.
+//  Copyright © 2017 aaron.zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
